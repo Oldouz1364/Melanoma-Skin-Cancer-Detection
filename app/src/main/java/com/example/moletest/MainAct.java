5f7f1e62af9f37ef6b79b2c51b5efc9531c4b21a
EvaAthanasiou <44544629+EvaAthanasiou@users.noreply.github.com>
2021-04-19 13:36:02 +0200

    login authentication - evangelia

---
+++ app/src/main/java/com/example/moletest/MainAct.java
@@ -0,0 +1,13 @@
+package com.example.moletest;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
