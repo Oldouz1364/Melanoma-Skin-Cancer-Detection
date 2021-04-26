565b276e8b8cbb17e89c2ca7c6010ff892ba3572
heloisevincent <helo.vincent1@gmail.com>
2021-04-26 13:12:08 +0200

    Color features

--- app/src/main/java/com/example/moletest/MainActivity.java
+++ app/src/main/java/com/example/moletest/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.moletest;
 
 import android.app.Activity;
@@ -13,10 +14,12 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.MatOfDouble;
 import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfInt4;
 import org.opencv.core.MatOfPoint;
@@ -31,6 +34,7 @@ import java.lang.Math;
 import java.io.IOException;
 import java.util.AbstractSequentialList;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
@@ -191,9 +195,10 @@ public class MainActivity extends AppCompatActivity {
         Imgproc.drawContours(img, contours,-1, color);
         */
 
-        Mat masked_img = new Mat(img.size(), CvType.CV_8UC3, new Scalar(255, 255, 255));
+        Mat masked_img = new Mat(img.size(), CvType.CV_8UC3, new Scalar(255, 255, 255)); // Bcs color image
         img.copyTo(masked_img, img_erode2);
 
+        /*
         // Display the image
         Bitmap img_bitmap = Bitmap.createBitmap(masked_img.cols(), masked_img.rows(),Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(masked_img,img_bitmap);
@@ -201,6 +206,8 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.img);
         imageView.setImageBitmap(img_bitmap);
+
+         */
  
         // Calculation of features
         if (img_erode2 != null) {
@@ -273,9 +280,176 @@ public class MainActivity extends AppCompatActivity {
             String str_var_distances = var_distances + "";
 
 
+            //Assymetry
+
+            // Finding the orientation of the blob
+            //https://stackoverflow.com/questions/14720722/binary-image-orientation
+            //https://en.wikipedia.org/wiki/Image_moment (Explanation on calculation of orientation with central moments)
+            double orientation;
+            if (M.mu20-M.mu02 != 0) {
+                orientation = 0.5 * Math.atan(2 * M.mu11 / (M.mu20 - M.mu02));
+                orientation = (orientation/Math.PI)*180; // Angle in degrees
+            }
+            else {
+                orientation = 0;
+            }
+            //Creating destination matrix
+            Mat rotated_mat = new Mat(row_size, column_size, img_erode2.type());
+            //Creating the transformation matrix
+            Mat rotationMatrix = Imgproc.getRotationMatrix2D(centroid, orientation, 1);
+            // Creating the object of the class Size
+            Size size = new Size(row_size, column_size);
+            // Rotating the given image https://www.tutorialspoint.com/how-to-rotate-an-image-with-opencv-using-java
+            Imgproc.warpAffine(img_erode2, rotated_mat, rotationMatrix, size);
+
+            // Dividing the image in top/bottom and right/left
+            Mat rotated_mat_top;
+            int centroid_pos_Y = (int) centroid.getY();
+            int centroid_pos_X = (int) centroid.getX();
+            Mat black_bottom = new Mat(row_size, column_size, CvType.CV_8UC1, new Scalar(0));
+            rotated_mat.copyTo(black_bottom);
+            Mat black_top = new Mat(row_size, column_size, CvType.CV_8UC1, new Scalar(0));
+            rotated_mat.copyTo(black_top);
+            Mat black_right = new Mat(row_size, column_size, CvType.CV_8UC1, new Scalar(0));
+            rotated_mat.copyTo(black_right);
+            Mat black_left = new Mat(row_size, column_size, CvType.CV_8UC1, new Scalar(0));
+            rotated_mat.copyTo(black_left);
+
+            // Manipulation of black_bottom
+            for (int i=centroid_pos_Y+1;i<=row_size;i++){
+                for (int j=0;j<= column_size;j++) {
+                    black_bottom.put(i, j, 0);
+                }
+            }
+            // Manipulation of black_top
+            for (int i=0;i<=centroid_pos_Y-1;i++){
+                for (int j=0;j<= column_size;j++) {
+                    black_top.put(i, j, 0);
+                }
+            }
+            // Manipulation of black_right
+            for (int i=0;i<=row_size;i++){
+                for (int j=centroid_pos_X+1;j<= column_size;j++) {
+                    black_right.put(i, j, 0);
+                }
+            }
+            // Manipulation of black_left
+            for (int i=0;i<=row_size;i++){
+                for (int j=0;j<= centroid_pos_X-1;j++) {
+                    black_left.put(i, j, 0);
+                }
+            }
+            // Calculating area of black_bottom
+            java.util.List<MatOfPoint> contour_black_bottom = new ArrayList<MatOfPoint>();
+            Imgproc.findContours(black_bottom, contour_black_bottom, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
+            // Second calculating the area of the contour
+            double Area_black_bottom = Imgproc.contourArea(contour_black_bottom.get(0));
+
+            // Calculating area of black_top
+            java.util.List<MatOfPoint> contour_black_top = new ArrayList<MatOfPoint>();
+            Imgproc.findContours(black_top, contour_black_top, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
+            // Second calculating the area of the contour
+            double Area_black_top = Imgproc.contourArea(contour_black_top.get(0));
+
+            // Calculating area of black_right
+            java.util.List<MatOfPoint> contour_black_right = new ArrayList<MatOfPoint>();
+            Imgproc.findContours(black_right, contour_black_right, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
+            // Second calculating the area of the contour
+            double Area_black_right = Imgproc.contourArea(contour_black_right.get(0));
+
+            // Calculating area of black_left
+            java.util.List<MatOfPoint> contour_black_left = new ArrayList<MatOfPoint>();
+            Imgproc.findContours(black_left, contour_black_left, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
+            // Second calculating the area of the contour
+            double Area_black_left = Imgproc.contourArea(contour_black_left.get(0));
+
+            double Ax = Math.abs(Area_black_top - Area_black_bottom);
+            double Ay = Math.abs(Area_black_left - Area_black_right);
+            double Assymetry = (Ax + Ay)/Area;
+            String str_test = Assymetry + "";
+
+
+            //Lesion Color Feature (LCF)
+
+            // RGB - Mean and standard deviation
+
+            // Splitting the masked bgr image in color channels
+            //List<Mat> bgr = new ArrayList<>();
+            //Core.split(masked_img, bgr);
+            //Mat blue = bgr.get(0);
+            //Mat green = bgr.get(1);
+            //Mat red = bgr.get(2);
+
+            MatOfDouble mean_masked_image= new MatOfDouble();
+            MatOfDouble std_masked_image= new MatOfDouble();
+
+            Core.meanStdDev(masked_img, mean_masked_image, std_masked_image);
+
+            Log.d("meanval1", String.valueOf(mean_masked_image.get(0,0)[0]));
+            Log.d("meanval2", String.valueOf(mean_masked_image.get(1,0)[0]));
+            Log.d("meanval3", String.valueOf(mean_masked_image.get(2,0)[0]));
+            Log.d("stdval1", String.valueOf(std_masked_image.get(0,0)[0]));
+            Log.d("stdval2", String.valueOf(std_masked_image.get(1,0)[0]));
+            Log.d("stdval3", String.valueOf(std_masked_image.get(2,0)[0]));
+
+            double blue_mean = mean_masked_image.get(0,0)[0];
+            double green_mean = mean_masked_image.get(1,0)[0];
+            double red_mean = mean_masked_image.get(2,0)[0];
+
+            double blue_std = std_masked_image.get(0,0)[0];
+            double green_std = std_masked_image.get(1,0)[0];
+            double red_std = std_masked_image.get(2,0)[0];
+
+            // Grayscale - Mean and standard deviation
+            Mat masked_gray_img = new Mat(imggray.size(), CvType.CV_8UC1, new Scalar(255));
+            imggray.copyTo(masked_gray_img, img_erode2);
+
+            MatOfDouble mean_masked_gray_image= new MatOfDouble();
+            MatOfDouble std_masked_gray_image= new MatOfDouble();
+
+            Core.meanStdDev(masked_gray_img, mean_masked_gray_image, std_masked_gray_image);
+
+            Log.d("meanval_gray", String.valueOf(mean_masked_gray_image.get(0,0)[0]));
+            Log.d("stdval_gray", String.valueOf(std_masked_gray_image.get(0,0)[0]));
+
+            double gray_mean = mean_masked_gray_image.get(0,0)[0];
+            double gray_std = std_masked_gray_image.get(0,0)[0];
+
+            // HSV - Mean and standard deviation
+            Mat masked_HSV_img = new Mat();
+            Imgproc.cvtColor(masked_img, masked_HSV_img, Imgproc.COLOR_BGR2HSV);
+
+            MatOfDouble mean_masked_HSV_image= new MatOfDouble();
+            MatOfDouble std_masked_HSV_image= new MatOfDouble();
+
+            Core.meanStdDev(masked_HSV_img, mean_masked_HSV_image, std_masked_HSV_image);
+
+            Log.d("meanval1_HSV", String.valueOf(mean_masked_HSV_image.get(0,0)[0]));
+            Log.d("meanval2_HSV", String.valueOf(mean_masked_HSV_image.get(1,0)[0]));
+            Log.d("meanval3_HSV", String.valueOf(mean_masked_HSV_image.get(2,0)[0]));
+            Log.d("stdval1_HSV", String.valueOf(std_masked_HSV_image.get(0,0)[0]));
+            Log.d("stdval2_HSV", String.valueOf(std_masked_HSV_image.get(1,0)[0]));
+            Log.d("stdval3_HSV", String.valueOf(std_masked_HSV_image.get(2,0)[0]));
+
+            double blue_mean_HSV = mean_masked_HSV_image.get(0,0)[0];
+            double green_mean_HSV = mean_masked_HSV_image.get(1,0)[0];
+            double red_mean_HSV = mean_masked_HSV_image.get(2,0)[0];
+
+            double blue_std_HSV = std_masked_HSV_image.get(0,0)[0];
+            double green_std_HSV = std_masked_HSV_image.get(1,0)[0];
+            double red_std_HSV = std_masked_HSV_image.get(2,0)[0];
+
+
+            // Display the image
+            Bitmap img_bitmap = Bitmap.createBitmap(masked_gray_img.cols(), masked_gray_img.rows(),Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(masked_gray_img,img_bitmap);
+
+
+            ImageView imageView = findViewById(R.id.img);
+            imageView.setImageBitmap(img_bitmap);
 
             TextView textView = findViewById(R.id.Feature);
-            textView.setText(str_var_distances);
+            textView.setText(str_test);
 
 
         }
